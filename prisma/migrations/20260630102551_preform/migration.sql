222405a5263ed737674eada1329fb5395145b547
ssenyonjoalvin <alvinssenyonjo9@gmail.com>
2026-06-30 20:32:31 +0300

    Remove broken preform migrations

--- prisma/migrations/20260630102551_preform/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `quiz` ALTER COLUMN `updatedAt` DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE `quizattempt` ALTER COLUMN `percentage` DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE `report` ALTER COLUMN `updatedAt` DROP DEFAULT;
